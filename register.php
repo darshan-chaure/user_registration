af6d8abd6d997dc13d82f695f8f1637abd8e9923
darshan chaure <darshan.chaure@esds.co.in>
2024-01-17 09:51:58 +0530

    Added header file

--- register.php
+++ register.php
@@ -1,11 +1,9 @@
 <html>
 <head>
     <title>Register</title>
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/script.js"></script>
+    <?php
+        include_once("includes/headers.php");
+    ?>
 </head>
 
 <body>
